f8b4f5651765e985f727552f9e2794f7c6cd7867
Sachin Mathur <samathur175@gmail.com>
2023-01-17 00:09:40 +0530

    New Todo List using react.
    added new task adding functnality and delete task.

---
+++ src/Components/NewTask.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+const NewTaskEl = () => {
+  return (
+    <div>
+      <h2 className="heading">ToDo List</h2>
+      <input
+        className="inputbox"
+        placeholder="Add Your Task Here"
+        id="input_box"
+      ></input>
+      <button
+        className="addBtn"
+        id="add_Btn"
+        onClick={() => {
+          let inputEl = document.getElementById("input_box");
+
+          if (inputEl.value == ``) {
+            alert("Add a task first");
+          } else {
+            let newTaskEl = document.createElement("input");
+            newTaskEl.value = inputEl.value;
+            newTaskEl.classList.add("newEl");
+
+            let editBtn = document.createElement("button");
+            editBtn.innerHTML = `Edit`;
+            editBtn.classList.add("editBtn");
+
+            let deleteBtnEl = document.createElement("button");
+            deleteBtnEl.innerHTML = `Delete`;
+            deleteBtnEl.classList.add("deleteBtn");
+
+            deleteBtnEl.addEventListener("click", () => {
+              document.body.removeChild(newTaskEl);
+              document.body.removeChild(editBtn);
+              document.body.removeChild(deleteBtnEl);
+            });
+
+            document.body.appendChild(newTaskEl);
+            document.body.appendChild(editBtn);
+            document.body.appendChild(deleteBtnEl);
+          }
+        }}
+      >
+        Add
+      </button>
+    </div>
+  );
+};
+
+export default NewTaskEl;
