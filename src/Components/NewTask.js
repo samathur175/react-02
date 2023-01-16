eac51ec790656c8afec201762dc43068a517027a
Sachin Mathur <samathur175@gmail.com>
2023-01-17 00:21:50 +0530

    Changer the location of the main funciton in the app.

--- src/Components/NewTask.js
+++ src/Components/NewTask.js
@@ -1,6 +1,36 @@
 import React from "react";
 
-const NewTaskEl = () => {
+function NewTaskEl() {
+  function NewTask__El() {
+    const inputEl = document.getElementById("input_box");
+
+    if (inputEl.value == ``) {
+      alert("Add a task first");
+    } else {
+      let newTaskEl = document.createElement("input");
+      newTaskEl.value = inputEl.value;
+      newTaskEl.classList.add("newEl");
+
+      let editBtn = document.createElement("button");
+      editBtn.innerHTML = `Edit`;
+      editBtn.classList.add("editBtn");
+
+      let deleteBtnEl = document.createElement("button");
+      deleteBtnEl.innerHTML = `Delete`;
+      deleteBtnEl.classList.add("deleteBtn");
+
+      deleteBtnEl.addEventListener("click", () => {
+        document.body.removeChild(newTaskEl);
+        document.body.removeChild(editBtn);
+        document.body.removeChild(deleteBtnEl);
+      });
+
+      document.body.appendChild(newTaskEl);
+      document.body.appendChild(editBtn);
+      document.body.appendChild(deleteBtnEl);
+    }
+  }
+
   return (
     <div>
       <h2 className="heading">ToDo List</h2>
@@ -9,43 +39,11 @@ const NewTaskEl = () => {
         placeholder="Add Your Task Here"
         id="input_box"
       ></input>
-      <button
-        className="addBtn"
-        id="add_Btn"
-        onClick={() => {
-          let inputEl = document.getElementById("input_box");
-
-          if (inputEl.value == ``) {
-            alert("Add a task first");
-          } else {
-            let newTaskEl = document.createElement("input");
-            newTaskEl.value = inputEl.value;
-            newTaskEl.classList.add("newEl");
-
-            let editBtn = document.createElement("button");
-            editBtn.innerHTML = `Edit`;
-            editBtn.classList.add("editBtn");
-
-            let deleteBtnEl = document.createElement("button");
-            deleteBtnEl.innerHTML = `Delete`;
-            deleteBtnEl.classList.add("deleteBtn");
-
-            deleteBtnEl.addEventListener("click", () => {
-              document.body.removeChild(newTaskEl);
-              document.body.removeChild(editBtn);
-              document.body.removeChild(deleteBtnEl);
-            });
-
-            document.body.appendChild(newTaskEl);
-            document.body.appendChild(editBtn);
-            document.body.appendChild(deleteBtnEl);
-          }
-        }}
-      >
+      <button className="addBtn" id="add_Btn" onClick={NewTask__El}>
         Add
       </button>
     </div>
   );
-};
+}
 
 export default NewTaskEl;
